c09b5fc355d5e614ea365475bb93020cf1c8ae2b
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-06-02 01:49:02 -0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,7 +4,7 @@
 import redis
 import requests
 r = redis.Redis()
-count = 3
+count = 0
 
 
 def get_page(url: str) -> str:
@@ -14,7 +14,7 @@ def get_page(url: str) -> str:
     r.set(f"cached:{url}", count)
     resp = requests.get(url)
     r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    r.setex(f"cached:{url}", 11, r.get(f"cached:{url}"))
     return resp.text
 
 
